861207d82378717c5863ae1e70a610c27ef6c98f
RednasR <72745166+RednasR@users.noreply.github.com>
2020-10-13 15:15:39 +0300

    Added all roles login validation

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,5 +1,8 @@
+const conf = require('../helpers/common');
+
 describe('Login functionality', function() {
-    
+    let d = new Date();
+
     it('Should display validation for empty user after attempted loggin', function () {
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
@@ -8,17 +11,87 @@ describe('Login functionality', function() {
     })
 
     it('Should be able to login with role User', function () {
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="Demo User"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="User"]').click()
-        cy.get('[type="submit"]').click()
+        cy.loginAsRole('User')
 
         cy.url().should('include', '/time-logging')
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
         cy.get('.tile.form').should('be.visible')
-        cy.get('.user-info__title').contains('Demo User')
+        cy.get('.user-info__title').contains(`${conf.USER_NAME}`)
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.attr', 'aria-current', 'true')
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
         cy.get('.main-nav').find('li').should('have.length', 1)
+        cy.get('.calendar--selected').should('have.class', 'calendar--today')
+        cy.get('.calendar--today').find('span').contains(d.getDate())
+        
+        cy.logOut()
+    })
+
+    it('Should be able to login with role Team Lead', function () {
+        cy.loginAsRole('Team Lead')
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains(`${conf.USER_NAME}`)
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.attr', 'aria-current', 'true')
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+        cy.get('.main-nav').find('li').should('have.length', 2)
+        cy.get('.calendar--selected').should('have.class', 'calendar--today')
+        cy.get('.calendar--today').find('span').contains(d.getDate())
+        
+        cy.logOut()
+    })
+
+    it('Should be able to login with role Manager', function () {
+        cy.loginAsRole('Manager')
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains(`${conf.USER_NAME}`)
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.attr', 'aria-current', 'true')
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.calendar--selected').should('have.class', 'calendar--today')
+        cy.get('.calendar--today').find('span').contains(d.getDate())
+        
+        cy.logOut()
+    })
+
+    it('Should be able to login with role Accountant', function () {
+        cy.loginAsRole('Accountant')
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains(`${conf.USER_NAME}`)
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.attr', 'aria-current', 'true')
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.calendar--selected').should('have.class', 'calendar--today')
+        cy.get('.calendar--today').find('span').contains(d.getDate())
+        
+        cy.logOut()
+    })
+
+    it('Should be able to login with role Admin', function () {
+        cy.loginAsRole('Admin')
+
+        cy.url().should('include', '/time-logging')
+        cy.get('.page__title').contains('Timesheets')
+        cy.get('.calendar').should('be.visible')
+        cy.get('.tile.form').should('be.visible')
+        cy.get('.user-info__title').contains(`${conf.USER_NAME}`)
+        cy.get('.main-nav').find('li').should('have.length', 6)
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.attr', 'aria-current', 'true')
+        cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+        cy.get('.calendar--selected').should('have.class', 'calendar--today')
+        cy.get('.calendar--today').find('span').contains(d.getDate())
+        
+        cy.logOut()
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+const conf = require('../helpers/common');
+
 const USER_ROLES_MAP = {
     "User": "",
     "Team Lead": "role=leads",
@@ -9,5 +11,20 @@ const USER_ROLES_MAP = {
 Cypress.Commands.add('loginAs', (role) => {
     window.localStorage.setItem('USER_INFO', '{"id":1,"firstName":"Demo","lastName":"User"}')
     window.localStorage.setItem('ACCESS_TOKEN', `userId=1&${USER_ROLES_MAP[role]}`)
+})
+
+Cypress.Commands.add('loginAsRole', (role) => {
+    cy.get('[id="loginForm.userId"]').click({force:true})
+    cy.get(`[aria-label="${conf.USER_NAME}"]`).click()
+    cy.get('[id="loginForm.role"]').click({force:true})
+    cy.get(`[aria-label="${role}"]`).click()
+    cy.get('[type="submit"]').click()
+    cy.wait(500)
+})
+
+Cypress.Commands.add('logOut', () => {
+    cy.get('.user-info__title').click()
+    cy.wait(500)
+    cy.get('[type="button"]').contains('Log Out').click()
 })
 
