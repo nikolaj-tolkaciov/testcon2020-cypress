a56cc117797131253a9baf8d30b098378ab7ee0d
Dumitru Grozavu <dumitru.grozavu@indeework.com>
2020-10-13 13:22:16 +0300

    select the current day

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -21,4 +21,11 @@ describe('Login functionality', function() {
         cy.get('.user-info__title').contains('TestCon User 1')
         cy.get('.main-nav').find('li').should('have.length', 2)
     })
+
+    it('Should select the current date', function () {
+        var d = new Date();
+        var currentDay = d.getDate();
+        cy.get('.calendar__date').contains(currentDay).click({force:true})
+        cy.get('.calendar--selected .calendar__date').contains(currentDay).should('be.visible')  
+    })
 })
