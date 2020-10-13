60ad3f9a646a835481b6e6bfa23e7f07c9e1cbbf
nk-kaia <52247086+nk-kaia@users.noreply.github.com>
2020-10-13 13:01:22 +0200

    Style update

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,13 +1,16 @@
 describe('Login functionality', function() {
 
     it('Should display validation for empty user after attempted loggin', function () {
+
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
+
     })
 
     it('Should be able to login with role Team Lead', function () {
+
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 6"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
@@ -20,12 +23,7 @@ describe('Login functionality', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 6')
         cy.get('.main-nav').find('li').should('have.length', 2)
-
-        const d = new Date();
-        const current_day = d.getDate();
-
-        cy.get('.calendar--today').should('contain', current_day)
-
+        
     })
 
 })
