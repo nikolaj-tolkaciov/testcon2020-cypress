1efc9c90cc28cec084b82528536de2cda8c9a86d
linakuch <lina.kuchalskyte@iterato.lt>
2020-10-13 16:12:21 +0300

    Update t-2.js
    t-2 fix Cr comments

--- cypress/integration/t-2.js
+++ cypress/integration/t-2.js
@@ -1,11 +1,4 @@
-describe('t-2 User role', function() {
-    
-    it('Should display validation for empty user after attempted loggin', function () {
-        cy.visit('/')
-        cy.get('.Select.not-valid').should('not.visible')
-        cy.get('[type="submit"]').click()
-        cy.get('.Select.not-valid').should('be.visible')
-    })
+describe('T-2 All roles', function() {
 
     it('Should be able to login with role User', function () {
         cy.visit('/')
@@ -25,20 +18,16 @@ describe('t-2 User role', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 22')
         cy.get('.main-nav').find('li').should('have.length', 1)
-    })
 
-    it('check time logging tab', function () {
         cy.get('.main-nav').find('li').find('a').should('have.class', 'main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
-    
-    it('Should display validation for empty user after attempted loggin', function () {
+
+    it('Should be able to login with role Team lead', function () {
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
-    })
 
-    it('Should be able to login with role Team lead', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 22"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
@@ -51,20 +40,16 @@ describe('t-2 User role', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 22')
         cy.get('.main-nav').find('li').should('have.length', 2)
-    })
 
-    it('check time logging tab', function () {
         cy.get('.main-nav').find('li').find('a').should('have.class', 'main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
-    
-    it('Should display validation for empty user after attempted loggin', function () {
+
+    it('Should be able to login with role Manager role', function () {
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
-    })
 
-    it('Should be able to login with role Manager role', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 22"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
@@ -77,20 +62,16 @@ describe('t-2 User role', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 22')
         cy.get('.main-nav').find('li').should('have.length', 5)
-    })
 
-    it('check time logging tab', function () {
         cy.get('.main-nav').find('li').find('a').should('have.class', 'main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
-    
-    it('Should display validation for empty user after attempted loggin', function () {
+
+    it('Should be able to login with role Admin', function () {
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
-    })
 
-    it('Should be able to login with role Admin', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 22"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
@@ -103,20 +84,16 @@ describe('t-2 User role', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 22')
         cy.get('.main-nav').find('li').should('have.length', 6)
-    })
 
-    it('check time logging tab', function () {
         cy.get('.main-nav').find('li').find('a').should('have.class', 'main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
-    
-    it('Should display validation for empty user after attempted loggin', function () {
+
+    it('Should be able to login with role Accountant', function () {
         cy.visit('/')
         cy.get('.Select.not-valid').should('not.visible')
         cy.get('[type="submit"]').click()
         cy.get('.Select.not-valid').should('be.visible')
-    })
-
-    it('Should be able to login with role Accountant', function () {
+        
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 22"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
@@ -129,9 +106,7 @@ describe('t-2 User role', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 22')
         cy.get('.main-nav').find('li').should('have.length', 5)
-    })
 
-    it('check time logging tab', function () {
         cy.get('.main-nav').find('li').find('a').should('have.class', 'main-nav__link--active').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
