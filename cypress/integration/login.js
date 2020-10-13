536581c297188ab0a36b8e3df0bec02a4dae6eff
Liutauras <liutauras.pilkionis@telesoftas.lt>
2020-10-13 16:29:55 +0300

    T-9

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,86 +1,92 @@
+import LoginPage from '../Objects/loginPageObject'
+import TimeLogging from '../Objects/timeLoggingObject'
+ 
+const loginPage = new LoginPage()
+const timeLogging = new TimeLogging()
+
 describe('Logging in with User role', function() {
     
     it('Should display validation for empty user after attempted loggin', function () {
-        cy.visit('/')
-        cy.get('.Select.not-valid').should('not.visible')
-        cy.get('[type="submit"]').click()
-        cy.get('.Select.not-valid').should('be.visible')
+        loginPage.visit()
+        loginPage.getUserValidationIndicator().should('not.visible')
+        loginPage.getLoginButton().click()
+        loginPage.getUserValidationIndicator().should('be.visible')
     })
 
     it('Should be able to login with role User', function () {
-        cy.visit('/')
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 17"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="User"]').click()
-        cy.get('[type="submit"]').click()
+        loginPage.visit()
+        loginPage.getLoginForm().click({force:true})
+        loginPage.getUserName("TestCon User 17").click()
+        loginPage.getLoginFormRole().click({force:true})
+        loginPage.getLoginRole("User").click()
+        loginPage.getLoginButton().click()
 
-        cy.get('.user-info__title').contains('TestCon User 17')
-        cy.get('.main-nav').find('li').should('have.length', 1)
-        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+        timeLogging.getUserTitle().contains('TestCon User 17')
+        timeLogging.getMainNavigationTab().find('li').should('have.length', 1)
+        timeLogging.getMainNavigationTab().find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
 
 describe('Logging in with Team Lead role', function() {
 
     it('Should be able to login with role Team Lead', function () {
-        cy.visit('/')
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 17"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="Team Lead"]').click()
-        cy.get('[type="submit"]').click()
+        loginPage.visit()
+        loginPage.getLoginForm().click({force:true})
+        loginPage.getUserName("TestCon User 17").click()
+        loginPage.getLoginFormRole().click({force:true})
+        loginPage.getLoginRole("Team Lead").click()
+        loginPage.getLoginButton().click()
 
-        cy.get('.user-info__title').contains('TestCon User 17')
-        cy.get('.main-nav').find('li').should('have.length', 2)
-        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+        timeLogging.getUserTitle().contains('TestCon User 17')
+        timeLogging.getMainNavigationTab().find('li').should('have.length', 2)
+        timeLogging.getMainNavigationTab().find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
 
 describe('Logging in with Manager role', function() {
 
     it('Should be able to login with role Manager', function () {
-        cy.visit('/')
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 17"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="Manager"]').click()
-        cy.get('[type="submit"]').click()
+        loginPage.visit()
+        loginPage.getLoginForm().click({force:true})
+        loginPage.getUserName("TestCon User 17").click()
+        loginPage.getLoginFormRole().click({force:true})
+        loginPage.getLoginRole("Manager").click()
+        loginPage.getLoginButton().click()
 
-        cy.get('.user-info__title').contains('TestCon User 17')
-        cy.get('.main-nav').find('li').should('have.length', 5)
-        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+        timeLogging.getUserTitle().contains('TestCon User 17')
+        timeLogging.getMainNavigationTab().find('li').should('have.length', 5)
+        timeLogging.getMainNavigationTab().find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
 
 describe('Logging in with Accountant role', function() {
 
     it('Should be able to login with role Accountant', function () {
-        cy.visit('/')
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 17"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="Accountant"]').click()
-        cy.get('[type="submit"]').click()
+        loginPage.visit()
+        loginPage.getLoginForm().click({force:true})
+        loginPage.getUserName("TestCon User 17").click()
+        loginPage.getLoginFormRole().click({force:true})
+        loginPage.getLoginRole("Accountant").click()
+        loginPage.getLoginButton().click()
 
-        cy.get('.user-info__title').contains('TestCon User 17')
-        cy.get('.main-nav').find('li').should('have.length', 5)
-        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+        timeLogging.getUserTitle().contains('TestCon User 17')
+        timeLogging.getMainNavigationTab().find('li').should('have.length', 5)
+        timeLogging.getMainNavigationTab().find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
 
 describe('Logging in with Admin role', function() {
 
     it('Should be able to login with role Admin', function () {
-        cy.visit('/')
-        cy.get('[id="loginForm.userId"]').click({force:true})
-        cy.get('[aria-label="TestCon User 17"]').click()
-        cy.get('[id="loginForm.role"]').click({force:true})
-        cy.get('[aria-label="Admin"]').click()
-        cy.get('[type="submit"]').click()
+        loginPage.visit()
+        loginPage.getLoginForm().click({force:true})
+        loginPage.getUserName("TestCon User 17").click()
+        loginPage.getLoginFormRole().click({force:true})
+        loginPage.getLoginRole("Admin").click()
+        loginPage.getLoginButton().click()
 
-        cy.get('.user-info__title').contains('TestCon User 17')
-        cy.get('.main-nav').find('li').should('have.length', 6)
-        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+        timeLogging.getUserTitle().contains('TestCon User 17')
+        timeLogging.getMainNavigationTab().find('li').should('have.length', 6)
+        timeLogging.getMainNavigationTab().find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
