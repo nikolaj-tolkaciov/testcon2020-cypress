38c97b6f5b103c07b0841860c02812e0a3d7e9bb
Liutauras <liutauras.pilkionis@telesoftas.lt>
2020-10-13 15:20:38 +0300

    T-2

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,4 +1,4 @@
-describe('Login functionality', function() {
+describe('User role', function() {
     
     it('Should display validation for empty user after attempted loggin', function () {
         cy.visit('/')
@@ -11,21 +11,99 @@ describe('Login functionality', function() {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 17"]').click()
         cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="User"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.get('.user-info__title').contains('TestCon User 17')
+        cy.get('.main-nav').find('li').should('have.length', 1)
+        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+    })
+})
+
+describe('Team Lead role', function() {
+    
+    it('Should display validation for empty user after attempted loggin', function () {
+        cy.visit('/')
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click()
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+
+    it('Should be able to login with role Team Lead', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 17"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
         cy.get('[aria-label="Team Lead"]').click()
         cy.get('[type="submit"]').click()
 
-        cy.url().should('include', '/time-logging')
-        cy.get('.page__title').contains('Timesheets')
-        cy.get('.calendar').should('be.visible')
-        cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 17')
         cy.get('.main-nav').find('li').should('have.length', 2)
+        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+    })
+})
+
+describe('Manager role', function() {
+    
+    it('Should display validation for empty user after attempted loggin', function () {
+        cy.visit('/')
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click()
+        cy.get('.Select.not-valid').should('be.visible')
     })
 
-    it('Selected day should be today', function () 
-    {
-        const day = new Date();
-        const todaysDate = day.getDate();
-        cy.get('.calendar--today').should('contain', todaysDate)
+    it('Should be able to login with role Manager', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 17"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Manager"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.get('.user-info__title').contains('TestCon User 17')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+    })
+})
+
+describe('Accountant role', function() {
+    
+    it('Should display validation for empty user after attempted loggin', function () {
+        cy.visit('/')
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click()
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+
+    it('Should be able to login with role Accountant', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 17"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Accountant"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.get('.user-info__title').contains('TestCon User 17')
+        cy.get('.main-nav').find('li').should('have.length', 5)
+        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
+    })
+})
+
+describe('Admin role', function() {
+    
+    it('Should display validation for empty user after attempted loggin', function () {
+        cy.visit('/')
+        cy.get('.Select.not-valid').should('not.visible')
+        cy.get('[type="submit"]').click()
+        cy.get('.Select.not-valid').should('be.visible')
+    })
+
+    it('Should be able to login with role Admin', function () {
+        cy.get('[id="loginForm.userId"]').click({force:true})
+        cy.get('[aria-label="TestCon User 17"]').click()
+        cy.get('[id="loginForm.role"]').click({force:true})
+        cy.get('[aria-label="Admin"]').click()
+        cy.get('[type="submit"]').click()
+
+        cy.get('.user-info__title').contains('TestCon User 17')
+        cy.get('.main-nav').find('li').should('have.length', 6)
+        cy.get('.main-nav').find('.main-nav__link--active').contains('Time Logging').should('have.css', 'color', 'rgb(64, 76, 237)')
     })
 })
