872f23238468597bce57050dea0f7ba6e851203e
EditaM <edita.maciuliene@agmis.lt>
2020-10-13 15:08:25 +0300

    t2 case

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,5 +1,7 @@
 const todaysDate = Cypress.moment().format('MMMM YYYY')
 const day = Cypress.moment().format('D')
+const roles= ['User','Team Lead', 'Manager', 'Accountant','Admin']
+const menu_items=[1,2,5,5,6]
 
 
 describe('Login functionality', function() {
@@ -17,7 +19,6 @@ describe('Login functionality', function() {
         cy.get('[id="loginForm.role"]').click({force:true})
         cy.get('[aria-label="Team Lead"]').click()
         cy.get('[type="submit"]').click()
-
         cy.url().should('include', '/time-logging')
         cy.get('.page__title').contains('Timesheets')
         cy.get('.calendar').should('be.visible')
@@ -26,5 +27,20 @@ describe('Login functionality', function() {
         cy.get('.calendar--today').contains(day)
         cy.get('.user-info__title').contains('TestCon User 5')
         cy.get('.main-nav').find('li').should('have.length', 2)
+    })
+
+    it('Should be able to login with all roles', function () {
+        for (let i = 0; i < 5; i++) {
+            cy.get('[id="loginForm.userId"]').click({force:true})
+            cy.get('[aria-label="TestCon User 5"]').click()
+            cy.get('[id="loginForm.role"]').click({force:true})
+            cy.get(`[aria-label="${roles[i]}"]`).click()
+            cy.get('[type="submit"]').click()
+            cy.get('.user-info__title').contains('TestCon User 5')
+            cy.get('.main-nav').find('li').should('have.length', menu_items[i])
+            cy.get('[aria-labelledby="timeLoggingId"]').should('have.css', 'color', 'rgb(64, 76, 237)')
+            cy.get('.user-info__title').click()
+            cy.get('.btn__list-item').contains('Log Out').click()
+        }
     })
 })
