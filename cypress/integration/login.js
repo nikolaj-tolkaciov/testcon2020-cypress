dab3ddfdbb7ed6ab9bf1c8e6b8bb24ffcd20e3d1
acraciun <acraciun@riasolutionsgroup.com>
2020-10-13 14:29:16 +0300

    TC1 - getCurrentDate

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -20,11 +20,11 @@ describe('Login functionality', function() {
         cy.get('.tile.form').should('be.visible')
         cy.get('.user-info__title').contains('TestCon User 8')
         cy.get('.main-nav').find('li').should('have.length', 2)
-
-    
     })
 
     it('Shoud display the date as current date', function (){
-        cy.get('.calendar--selected').should('have.class','calendar--today')
+        const date = new Date();
+        const todayDate = date.getDate();
+        cy.get('.calendar--today').should('contain', todayDate)
     })
 })
