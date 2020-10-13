db2f965ee1dc0e8353aa3ae59ba8f21711e5d3d9
Maksim Trojan <maksim.trojan@asseco.lt>
2020-10-13 12:11:29 +0300

    delete comment

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -7,21 +7,6 @@ describe('Login functionality', function() {
         cy.get('.Select.not-valid').should('be.visible')
     })
 
-    // it('Should be able to login with role User', function () {
-    //     cy.get('[id="loginForm.userId"]').click({force:true})
-    //     cy.get('[aria-label="Demo User"]').click()
-    //     cy.get('[id="loginForm.role"]').click({force:true})
-    //     cy.get('[aria-label="User"]').click()
-    //     cy.get('[type="submit"]').click()
-
-    //     cy.url().should('include', '/time-logging')
-    //     cy.get('.page__title').contains('Timesheets')
-    //     cy.get('.calendar').should('be.visible')
-    //     cy.get('.tile.form').should('be.visible')
-    //     cy.get('.user-info__title').contains('Demo User')
-    //     cy.get('.main-nav').find('li').should('have.length', 1)
-    // })
-
     it('Should be able to login with role Team Lead', function () {
         cy.get('[id="loginForm.userId"]').click({force:true})
         cy.get('[aria-label="TestCon User 25"]').click()
